2d8396ad889480704effb2ca34f7be9e9eaa8db5
Jayesh Gadhok <jayeshgadhok@gmail.com>
2024-05-06 10:54:53 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+const index = () => {
+	return (
+		<View className="flex-1 items-center justify-center bg-red-500">
+			<Text>index</Text>
+		</View>
+	);
+};
+
+export default index;
